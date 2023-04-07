18a876f1f361733013e2189bfe6b4300608aa320
Olena Hrechykhina <117777710+Elena-Hrechyk@users.noreply.github.com>
2023-04-06 23:48:32 -0700

    create files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+let lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player/src/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const LOCAL_KEY = 'videoplayer-current-time';
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(evt) {
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(evt.seconds));
+}
+
+const getCurrentTime = JSON.parse(localStorage.getItem(LOCAL_KEY));
+console.log(getCurrentTime);
+
+player.setCurrentTime(getCurrentTime);
